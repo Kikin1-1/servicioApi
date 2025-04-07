7311a2a690fad2fdfb8932cb4555cf9bd3f1469d
Enrque Sandoval <enrique.sandoval2024@gmail.com>
2025-04-07 13:49:52 -0700

    base de data

--- module/model.js
+++ module/model.js
@@ -1,23 +1,15 @@
 // module/model.js
 // module/model.js
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
-// Esta función genera una nueva conexión cada vez que se llama
+// Esta función devuelve una conexión lista para usarse
 export async function getConnection() {
   return await mysql.createConnection({
-    host: '72.167.57.169',
-    user: 'kikin',
-    password: 'Wowmimis123',
-    database: 'patrick',
+    host: "72.167.57.169", // IP de tu BD en GoDaddy
+    user: "kikin",
+    password: "Wowmimis123",
+    database: "patrick",
     port: 3306
   });
 }
-db.connect(err => {
-  if (err) {
-    console.error('❌ Error al conectar con la base de datos:', err.message);
-  } else {
-    console.log('✅ Conectado exitosamente a la base de datos');
-  }
-});
 
-export default db;
